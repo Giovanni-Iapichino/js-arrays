30daaa8d96eea719564a8e860c8633c9aa0aa041
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-03 18:00:35 +0200

    fix-console-fabio

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -41,9 +41,10 @@ for (let i = 0; i < teachers.length; i++) {
   const currentTeachers = teachers[i];
   if (currentTeachers === "Fabio") {
     isFabioPresent = true;
-    console.table(isFabioPresent);
   }
 }
+
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 let teachersString = "";
