a1bf3cc8dc2cf28262e23987b81c3b2c823d0b85
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-03 17:51:51 +0200

    fix-02-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -6,8 +6,10 @@ const reversedTeachers = [];
 for (let i = teachers.length - 1; i >= 0; i--) {
   const currentTeachers = teachers[i];
   reversedTeachers.push(currentTeachers);
-  console.log(reversedTeachers);
 }
+
+console.log(reversedTeachers);
+
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
@@ -16,9 +18,10 @@ for (let i = 0; i < teachers.length; i++) {
   const currentTeachers = teachers[i];
   if (currentTeachers.length >= 5) {
     longNames.push(currentTeachers);
-    console.log(longNames);
   }
 }
+
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 for (let i = 0; i < teachers.length; i++) {
@@ -30,14 +33,12 @@ for (let i = 0; i < teachers.length; i++) {
 }
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+let isFabioPresent = false;
 
 for (let i = 0; i < teachers.length; i++) {
   const currentTeachers = teachers[i];
   if (currentTeachers === "Fabio") {
-    const isFabioPresent = true;
-    console.table(isFabioPresent);
-  } else {
-    const isFabioPresent = false;
+    isFabioPresent = true;
     console.table(isFabioPresent);
   }
 }
@@ -49,6 +50,7 @@ for (let i = 0; i < teachers.length; i++) {
   teachersString += teachers[i];
   if (i < teachers.length - 1) {
     teachersString += ", ";
-    console.log(teachersString);
   }
 }
+
+console.log(teachersString);
